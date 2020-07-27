ab7bf29ed944c2515b2fecdaa22565bce47829d0
Richard Perez <PEREZR27@my.erau.edu>
2020-07-27 11:29:42 -0500

    Updates to Task Tracker

--- TaskTracker/Program.cs
+++ TaskTracker/Program.cs
@@ -24,36 +24,41 @@ namespace TaskTracker
 
         public static void programStart(List<string> Tasks)
         {
-            int commands = int.Parse(Console.ReadLine());
-            switch(commands)
-            {
-                case 1:
-                    AddTask(Tasks);
-                    break;
-                case 2:
-                    ViewTask(Tasks);
-                    break;
-                case 3:
-                    Environment.Exit(0);
-                    break;
-             
-            }
-        }
-
-        private static void ViewTask(List<string> Tasks)
-        {
-            
-        }
-
-        private static void AddTask(List<string> Tasks)
-        {
             do
+            {
+                int commands = int.Parse(Console.ReadLine());
+                switch (commands)
+                {
+                    case 1:
+                        AddTask(Tasks);
+                        break;
+                    case 2:
+                        ViewTask(Tasks);
+                        break;
+                    case 3:
+                        Environment.Exit(0);
+                        break;
+
+                }
+            }
+            while (Tasks.Count != 0);
+            Console.WriteLine("List Complete!");
+        }
+
+        public static void ViewTask(List<string> Tasks)
+        {
+            Tasks.ForEach(Console.WriteLine);
+        }
+
+        public static void AddTask(List<string> Tasks)
+        {
+           do
             {
                 Console.WriteLine("Add a Task to your existing list\n");
                 Console.WriteLine("Enter nothing to exit");
                 Tasks.Add($"{Console.ReadLine()}");
             }
-            while (Console.ReadLine() != "");
+            while (Console.ReadLine() != "") ;
         }
     }
 }
