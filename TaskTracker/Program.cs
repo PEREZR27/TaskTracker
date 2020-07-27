f0cb870396a189bf8a5583110ead23923c4b9cb8
Richard Perez <PEREZR27@my.erau.edu>
2020-07-27 09:43:26 -0500

    Add project files.

---
+++ TaskTracker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace TaskTracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> strList = New List<string>();
+        }
+    }
+}
+
